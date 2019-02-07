29266dc65d0d9b3a80c828d5e3f38be8022173e9
Joshua Taillon <jat255@gmail.com>
2019-02-06 22:19:58 -0500

    change environment variables

---
+++ etc/restic/restic_env.sh
@@ -0,0 +1,6 @@
+# environment variables for backup repository.
+# see https://restic.readthedocs.io/en/latest/040_backup.html#environment-variables
+# for details
+
+export RESTIC_REPOSITORY="repo:path/to/repo"
+export RESTIC_PASSWORD="password"
